99e9685e3235785e906d2a58179f6498633375eb
Aliaksandr Marynovich <aliaksandr_marynovich@epam.com>
2018-11-11 11:43:17 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const API_KEY = '7719cc235c3c4a4381f84089ece47f5f'; 
+const BASE_URL = 'https://newsapi.org/v2/';
+
+const response = await fetch(`${BASE_URL}?apiKey=${API_KEY}`);
+console.log('2');
+
+debugger;
+
