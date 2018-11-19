b469dea05c8b9a3280e64612c97baebe4ec79313
Aliaksandr Marynovich <Aliaksandr_Marynovich@epam.com>
2018-11-19 11:17:08 +0300

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9fbffaa02cc906d6be96";
+/******/ 	var hotCurrentHash = "96e82cd97eb6ac0d6d11";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -2258,41 +2258,15 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var NewsData = /** @class */ (function () {
     function NewsData(toggleElement, getItemElement, getTopRatedNewsListAsync) {
-        this.sourcesMap = new Map();
-        this.topRatedNews = new Map();
-        this.currentSelectedSource = null;
-        this.currentHeadlinesItem = null;
-        // toggle element selection
-        this.toggleItemSelection = toggleElement;
-        // checks whether selected source item is 'li' element, otherwise finds it
-        this.getItemElement = getItemElement;
-        // TODO: add comment
-        this.getTopRatedNewsListAsync = getTopRatedNewsListAsync;
-    }
-    Object.defineProperty(NewsData.prototype, "sources", {
-        get: function () {
-            return this.sourcesMap;
-        },
-        set: function (sources) {
-            var _this = this;
-            sources.forEach(function (sourceItem) { return _this.sourcesMap.set(sourceItem.id, sourceItem); });
-        },
-        enumerable: true,
-        configurable: true
-    });
-    NewsData.prototype.setTopRatedNews = function (news) {
-        return this.topRatedNews.set(news[0].source.id, news);
-    };
-    /**
-     *
-     * @param {string} id
-     * @returns {Promise<Map<string,{}>>}
-     */
-    NewsData.prototype.getTopRatedNewsById = function (id) {
-        return __awaiter(this, void 0, void 0, function () {
+        var _this = this;
+        this.setSources = function (sources) { return sources.forEach(function (sourceItem) { return _this.sourcesMap.set(sourceItem.id, sourceItem); }); };
+        this.getSources = function () { return _this.sourcesMap; };
+        // tslint:disable-next-line:no-any
+        this.setTopRatedNews = function (news) { return _this.topRatedNews.set(news[0].source.id, news); };
+        this.getTopRatedNewsById = function (id) { return __awaiter(_this, void 0, void 0, function () {
             var topRatedNews;
             return __generator(this, function (_a) {
                 switch (_a.label) {
@@ -2306,8 +2280,18 @@ var NewsData = /** @class */ (function () {
                         return [2 /*return*/, this.setTopRatedNews(topRatedNews).get(id)];
                 }
             });
-        });
-    };
+        }); };
+        this.sourcesMap = new Map();
+        this.topRatedNews = new Map();
+        this.currentSelectedSource = null;
+        this.currentHeadlinesItem = null;
+        // toggle element selection
+        this.toggleItemSelection = toggleElement;
+        // checks whether selected source item is 'li' element, otherwise finds it
+        this.getItemElement = getItemElement;
+        // TODO: add comment
+        this.getTopRatedNewsListAsync = getTopRatedNewsListAsync;
+    }
     /**
      * Toggles element selection and set current source item
      * @param {EventTarget} eventTarget
@@ -2340,7 +2324,7 @@ exports.NewsData = NewsData;
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 exports.API_KEY = '7719cc235c3c4a4381f84089ece47f5f';
 exports.API_VERSION = 'v2';
 exports.BASE_URL = 'https://newsapi.org';
@@ -2368,7 +2352,7 @@ module.exports = __webpack_require__.p + "images/default-news.png";
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var es6_promise_1 = __webpack_require__(/*! es6-promise */ "./node_modules/es6-promise/dist/es6-promise.js");
 var init_1 = __webpack_require__(/*! init */ "./src/init.ts");
 __webpack_require__(/*! styles.css */ "./src/styles.css");
@@ -2389,33 +2373,87 @@ init_1.init();
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
-var NewsData_1 = __webpack_require__(/*! ./NewsData */ "./src/NewsData.ts");
-var index_1 = __webpack_require__(/*! ./services/index */ "./src/services/index.ts");
-var index_2 = __webpack_require__(/*! ./utils/index */ "./src/utils/index.ts");
-exports.init = function () {
-    var sourceList = document.getElementById('sources-list');
-    var sourceItem = document.getElementById('sources-item');
-    var topRatedSection = document.getElementById('top-rated');
-    var topRatedList = document.getElementById('top-rated-list');
-    var topRatedItem = document.getElementById('top-rated-item');
-    var showTopRatedArticlesBlock = function () { return topRatedSection.classList.remove('hidden'); };
-    var toggleItem = function (element) { return element.classList.toggle('selected'); };
-    var newsData = new NewsData_1.NewsData(toggleItem, index_2.getItemElement, index_1.getTopHeadlinesAsync);
-    // first get the source data
-    index_1.getSourcesAsync().then(function (sources) {
-        newsData.sources = sources;
-        index_1.renderList(sourceList, sourceItem, newsData.sources);
-    });
-    sourceList.addEventListener('click', showTopRatedArticlesBlock, { once: true });
-    // add event listener to top rated articles item.
-    sourceList.addEventListener('click', function (_a) {
-        var item = _a.target;
-        newsData.toggleSourceItem(item);
-        newsData.getTopRatedNewsById(index_2.getElementId(item.id))
-            .then(function (articles) { return index_1.renderList(topRatedList, topRatedItem, articles); });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+exports.__esModule = true;
+var NewsData_1 = __webpack_require__(/*! NewsData */ "./src/NewsData.ts");
+var services_1 = __webpack_require__(/*! services */ "./src/services/index.ts");
+var utils_1 = __webpack_require__(/*! utils */ "./src/utils/index.ts");
+exports.init = function () { return __awaiter(_this, void 0, void 0, function () {
+    var sourceList, sourceItem, topRatedSection, topRatedList, topRatedItem, showTopRatedArticlesBlock, toggleItem, newsData, sources;
+    var _this = this;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                sourceList = document.getElementById('sources-list');
+                sourceItem = document.getElementById('sources-item');
+                topRatedSection = document.getElementById('top-rated');
+                topRatedList = document.getElementById('top-rated-list');
+                topRatedItem = document.getElementById('top-rated-item');
+                showTopRatedArticlesBlock = function () { return topRatedSection.classList.remove('hidden'); };
+                toggleItem = function (element) { return element.classList.toggle('selected'); };
+                newsData = new NewsData_1.NewsData(toggleItem, utils_1.getItemElement, services_1.getTopHeadlinesAsync);
+                return [4 /*yield*/, services_1.getSourcesAsync()];
+            case 1:
+                sources = _a.sent();
+                newsData.setSources(sources);
+                services_1.renderList(sourceList, sourceItem, newsData.getSources());
+                sourceList.addEventListener('click', showTopRatedArticlesBlock, { once: true });
+                sourceList.addEventListener('click', function (_a) {
+                    var item = _a.target;
+                    return __awaiter(_this, void 0, void 0, function () {
+                        var articles;
+                        return __generator(this, function (_b) {
+                            switch (_b.label) {
+                                case 0:
+                                    newsData.toggleSourceItem(item);
+                                    return [4 /*yield*/, newsData.getTopRatedNewsById(utils_1.getElementId(item.id))];
+                                case 1:
+                                    articles = _b.sent();
+                                    services_1.renderList(topRatedList, topRatedItem, articles);
+                                    return [2 /*return*/];
+                            }
+                        });
+                    });
+                });
+                return [2 /*return*/];
+        }
+    });
+}); };
 
 
 /***/ }),
@@ -2474,12 +2512,12 @@ var __rest = (this && this.__rest) || function (s, e) {
     return t;
 };
 var _this = this;
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var constants_1 = __webpack_require__(/*! ./constants */ "./src/constants.ts");
 /**
  * Returns NewsAPI response
- * @param {string} endpoint -
- * @param {string} [parameters] -
+ * @param endpoint -
+ * @param parameters -
  */
 exports.request = function (endpoint, parameters) {
     if (parameters === void 0) { parameters = ''; }
@@ -2520,7 +2558,7 @@ exports.request = function (endpoint, parameters) {
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 __export(__webpack_require__(/*! ./newsApi */ "./src/services/newsApi.ts"));
 __export(__webpack_require__(/*! ./renderList */ "./src/services/renderList.ts"));
 
@@ -2572,9 +2610,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var request_1 = __webpack_require__(/*! ../request */ "./src/request.ts");
-exports.getSourcesAsync = function () { return __awaiter(_this, void 0, void 0, function () {
+exports.getSourcesAsync = function () { return __awaiter(_this, void 0, Promise, function () {
     var sources;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -2589,7 +2627,7 @@ exports.getSourcesAsync = function () { return __awaiter(_this, void 0, void 0, 
  *
  * @param {string} sources
  */
-exports.getTopHeadlinesAsync = function (sourceId) { return __awaiter(_this, void 0, void 0, function () {
+exports.getTopHeadlinesAsync = function (sourceId) { return __awaiter(_this, void 0, Promise, function () {
     var articles;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -2613,8 +2651,8 @@ exports.getTopHeadlinesAsync = function (sourceId) { return __awaiter(_this, voi
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = __webpack_require__(/*! ../utils/index */ "./src/utils/index.ts");
+exports.__esModule = true;
+var utils_1 = __webpack_require__(/*! ../utils */ "./src/utils/index.ts");
 /**
  * Renders item's list and replaces the gotten by id list with it
  * @param {HTMLElement} list
@@ -2625,13 +2663,13 @@ exports.renderList = function (list, item, data) {
     var documentFragmentList = document.createDocumentFragment();
     data.forEach(function (element) {
         var newItem = item.cloneNode(true);
-        var children = index_1.flattenChildNodes(newItem);
+        var children = utils_1.flattenChildNodes(newItem);
         // TODO: need to create a separate service(s)
         children.forEach(function (item) {
             switch (item.id) {
                 // for source items
                 case 'source-image':
-                    item.src = index_1.generateLogoUrl(element.url);
+                    item.src = utils_1.generateLogoUrl(element.url);
                     break;
                 case 'source-caption':
                     item.textContent = element.name;
@@ -2652,9 +2690,9 @@ exports.renderList = function (list, item, data) {
                 default: return;
             }
             // add id postfix only for items with id
-            item.id = item.id && item.id + "_" + (element.id || index_1.generateUniqueId());
+            item.id = item.id && item.id + "_" + (element.id || utils_1.generateUniqueId());
         });
-        newItem.id = item.id + "_" + (element.id || index_1.generateUniqueId());
+        newItem.id = item.id + "_" + (element.id || utils_1.generateUniqueId());
         documentFragmentList.append(newItem);
     });
     // remove previous added items
@@ -2688,11 +2726,10 @@ exports.renderList = function (list, item, data) {
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 /**
  *
  * @param {HTMLElement} element
- * @returns {HTMLCollection}
  */
 function flattenChildNodes(element) {
     var children = Array.from(element.children);
@@ -2717,7 +2754,7 @@ exports.flattenChildNodes = flattenChildNodes;
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 /**
  * Returns URL for logo icon
  * @param {string} url
@@ -2740,7 +2777,7 @@ exports.generateLogoUrl = function (url, size) {
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 exports.generateUniqueId = function () { return Math.random().toString(36).substr(2, 16); };
 
 
@@ -2755,11 +2792,10 @@ exports.generateUniqueId = function () { return Math.random().toString(36).subst
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 /**
- *
- * @param {string} id
- * @returns {string}
+ * Returns id
+ * @param id
  */
 function getElementId(id) {
     return id.split('_')[1];
@@ -2778,11 +2814,7 @@ exports.getElementId = getElementId;
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- *
- * @param {*} element
- */
+exports.__esModule = true;
 exports.getItemElement = function (element) {
     if (element.tagName.match(/li/i)) {
         return element;
@@ -2808,12 +2840,12 @@ exports.getItemElement = function (element) {
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 __export(__webpack_require__(/*! ./flattenChildNodes */ "./src/utils/flattenChildNodes.ts"));
 __export(__webpack_require__(/*! ./generateLogoUrl */ "./src/utils/generateLogoUrl.ts"));
+__export(__webpack_require__(/*! ./generateUniqueId */ "./src/utils/generateUniqueId.ts"));
 __export(__webpack_require__(/*! ./getElementId */ "./src/utils/getElementId.ts"));
 __export(__webpack_require__(/*! ./getSourceItemElement */ "./src/utils/getSourceItemElement.ts"));
-__export(__webpack_require__(/*! ./generateUniqueId */ "./src/utils/generateUniqueId.ts"));
 
 
 /***/ })
