1f2df0b60203f894566763869631d70df5258223
Aliaksandr Marynovich <Aliaksandr_Marynovich@epam.com>
2018-11-19 12:50:46 +0300

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b364fe2d657ea4dde429";
+/******/ 	var hotCurrentHash = "f3b5de05e409e3eb6932";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
